fc97e49f9f5e007093182188b0481063352c44e3
yibin <mayibin001@163.com>
2019-04-08 16:06:33 +0800

    测试上传项目至github

---
+++ Sample/Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        ///// <summary>
+        ///// 显示首页
+        ///// </summary>
+        ///// <returns></returns>
+        //public IActionResult Index()
+        //{
+        //    //return View();            
+        //    var contents = new List<Content>();
+        //    for (int i = 1; i < 11; i++)
+        //    {
+        //        contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+        //    }
+        //    return View(new ContentViewModel { Contents = contents });
+        //}
+
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel {
+                Contents = new List<Content>
+                { contents}
+                });
+        }
+    }
+}
